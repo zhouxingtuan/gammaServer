05ec141f3a610cb962a429fa2117f514ee33223a
ZhouXingTuan <appletreezxt@qq.com>
2017-04-02 16:05:40 +0800

    c

--- src/libcore/globalsetting.cpp
+++ src/libcore/globalsetting.cpp
@@ -20,7 +20,7 @@ GlobalSetting::GlobalSetting(void) : RefObject(),
 		m_acceptReadArr[i] = NULL;
 		m_acceptReceiveArr[i] = NULL;
 	}
-	memset(m_key, 0, sizeof(m_key));
+	memset(m_key, 0, NET_KEY_LENGTH);
 }
 GlobalSetting::~GlobalSetting(void){
 	GlobalSetting::destroy();
