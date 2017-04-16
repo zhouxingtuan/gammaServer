af5d8b72b3ac9ab1e44392f8647020e92a03d911
ZhouXingTuan <appletreezxt@qq.com>
2017-04-16 17:15:37 +0800

    c

--- src/libcore/globalhandler.h
+++ src/libcore/globalhandler.h
@@ -15,8 +15,8 @@
 NS_HIVE_BEGIN
 
 typedef DestinationGroup<Handler> HandlerDestinationGroup;
-typedef HandlerDestinationGroup::CreateFunction CreateFunctionHandler;
-typedef HandlerDestinationGroup::DestroyFunction DestroyFunctionHandler;
+typedef typename HandlerDestinationGroup::CreateFunction CreateFunctionHandler;
+typedef typename HandlerDestinationGroup::DestroyFunction DestroyFunctionHandler;
 
 class GlobalHandler : public RefObject, public Sync
 {
