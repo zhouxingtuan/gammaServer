4f621255e0c34ee71759043170768c6055ad6d66
ZhouXingTuan <appletreezxt@qq.com>
2017-04-16 14:28:46 +0800

    c

--- src/libcore/globalhandler.h
+++ src/libcore/globalhandler.h
@@ -41,7 +41,7 @@ public:
 
 	template<class _OBJECT_>
 	_OBJECT_* getDestination(uint32 handle){
-		return static_cast<_OBJECT_*>(void*)(m_pGroup->getDestination(handle));
+		return static_cast<_OBJECT_*>((void*)(m_pGroup->getDestination(handle)));
 	}
 	DestinationGroup* getGroup(void) { return m_pGroup; }
 	void initialize(void);
