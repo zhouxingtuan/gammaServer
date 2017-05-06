35c4ada0d067ab4189a741c42c84670580d75d1b
ZhouXingTuan <appletreezxt@qq.com>
2017-05-06 11:12:10 +0800

    c

--- src/libcore/handler.cpp
+++ src/libcore/handler.cpp
@@ -194,11 +194,11 @@ void Handler::acceptTask(Task* pTask){
 	m_taskQueue.push_back(pTask);
 	if( m_isInHandlerQueue ){	// 已经在队列里面，入队后不做任何处理
 		this->unlock();
-		LOG_DEBUG("Handler acceptTask when is in HandlerQueue");
+		LOG_DEBUG("Handler acceptTask when is in HandlerQueue handle=%d", getHandle());
 	}else{						// 原先不在队列中则重新入队
 		m_isInHandlerQueue = true;
 		this->unlock();
-		LOG_DEBUG("Handler acceptTask when is not in HandlerQueue, and push");
+		LOG_DEBUG("Handler acceptTask when is not in HandlerQueue, and push handle=%d", getHandle());
 		HandlerQueue::getInstance()->acceptHandler(this);	// 将handler提交给队
 	}
 }
